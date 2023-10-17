b894af4725f01815d430384f115eb8ed695a7eb5
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-10-17 20:06:12 +0530

    Add files via upload

---
+++ Info_Collector.py
@@ -0,0 +1,41 @@
+import re
+
+def get_user_info():
+    print("Welcome! Please provide the following information:")
+    
+    while True:
+        user_name = input("Name: ")
+        if user_name.strip():
+            break
+        else:
+            print("Name cannot be empty. Please try again.")
+    
+    while True:
+        user_age = input("Age: ")
+        if user_age.isdigit() and 0 <= int(user_age) <= 120:
+            user_age = int(user_age)
+            break
+        else:
+            print("Age must be a valid number between 0 and 120. Please try again.")
+    
+    while True:
+        user_email = input("Email: ")
+        email_pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if re.match(email_pattern, user_email):
+            break
+        else:
+            print("Invalid email format. Please try again.")
+    
+    return {
+        "Name": user_name,
+        "Age": user_age,
+        "Email": user_email
+    }
+
+if __name__ == "__main__":
+    user_info = get_user_info()
+    
+    print("\nThank you for providing your information!")
+    print("Name:", user_info["Name"])
+    print("Age:", user_info["Age"])
+    print("Email:", user_info["Email"])
